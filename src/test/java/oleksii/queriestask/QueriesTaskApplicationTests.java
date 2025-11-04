b780e3bb5c35b1884cd0e4f0d5fdd25da8a2ff42
Bilous Oleksii <bilouole@fit.cvut.cz>
2025-11-04 15:12:11 +0100

    Initial commit

---
+++ src/test/java/oleksii/queriestask/QueriesTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package oleksii.queriestask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueriesTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
