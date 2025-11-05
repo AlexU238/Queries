1558a06e0b439d59e950103fec7391c59f86e82d
Bilous Oleksii <bilouole@fit.cvut.cz>
2025-11-05 18:30:05 +0100

    Added Lombok and changed Query to use Lombok annotations

--- src/main/java/oleksii/queriestask/datamodel/Query.java
+++ src/main/java/oleksii/queriestask/datamodel/Query.java
@@ -1,29 +1,17 @@
 package oleksii.queriestask.datamodel;
 
+import lombok.*;
 import org.springframework.stereotype.Component;
 
-import java.nio.file.LinkOption;
-
 @Component
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Query {
 
     private long id;
 
     private String query;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getQuery() {
-        return query;
-    }
-
-    public void setQuery(String query) {
-        this.query = query;
-    }
 }
