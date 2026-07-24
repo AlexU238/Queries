a3da1927edf8125def8528e93319d92642284c7b
Bilous Oleksii <bilouole@fit.cvut.cz>
2026-07-24 19:54:58 +0200

    Updated the QueryService interface

--- src/main/java/oleksii/queriestask/service/QueryService.java
+++ src/main/java/oleksii/queriestask/service/QueryService.java
@@ -4,10 +4,13 @@ import oleksii.queriestask.datamodel.Query;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public interface QueryService {
 
     Long addQuery(Query query);
+
+    Optional<Query> getQueryById(Long id);
 
     Collection<Query> getQueries();
 
